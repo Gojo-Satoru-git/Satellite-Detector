2fa6639b95309a250522701057b7ff12efaa12c1
Praveen kumar <160558289+Gojo-Satoru-git@users.noreply.github.com>
2025-05-07 17:17:31 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://spacecraft-backend.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+   server: {
+    host: '0.0.0.0',  // Exposes to external network
+    port: 3000         // Specify the port you want
+   }
 })
